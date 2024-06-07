a8f2100373d8d16a9979ea9ab5bddaa2e357e1e2
Cris Vasquez <cfiguer7@ucsc.edu>
2024-06-07 00:27:04 -0700

    added configuration

--- ece218Robot.X/nbproject/Makefile-variables.mk
+++ ece218Robot.X/nbproject/Makefile-variables.mk
@@ -8,3 +8,7 @@ CND_BASEDIR=`pwd`
 CND_ARTIFACT_DIR_default=dist/default/production
 CND_ARTIFACT_NAME_default=ece218Robot.X.production.hex
 CND_ARTIFACT_PATH_default=dist/default/production/ece218Robot.X.production.hex
+# ROBOT_TEST configuration
+CND_ARTIFACT_DIR_ROBOT_TEST=dist/ROBOT_TEST/production
+CND_ARTIFACT_NAME_ROBOT_TEST=ece218Robot.X.production.null
+CND_ARTIFACT_PATH_ROBOT_TEST=dist/ROBOT_TEST/production/ece218Robot.X.production.null
